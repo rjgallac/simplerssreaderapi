fdf9c9c7eb4a389bad865ad6f2a7d98f4d35ad8e
rob <rjgallac@hotmail.com>
2020-09-15 21:04:40 +0100

    initial setup and POC

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const request = require('request');
+var parseString = require('xml2js').parseString;
+
+const app = express()
+const port = 3000
+
+feeds = [
+    'https://www.hotukdeals.com/rss/all'
+]
+
+app.get('/', (req, res) => {
+  request(feeds[0], { json: true }, (err, response, body) => {
+    if (err) { return console.log(err); }
+        parseString(body, function (err, result) {
+            res.json(result)
+        });
+    });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
