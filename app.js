7350dc77664191fda7420532a0a8eeec4ed81dec
rob <rjgallac@hotmail.com>
2020-09-16 22:31:54 +0100

    cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const app = express()
+var cors = require('cors');
+const app = express();
+app.use(cors());
 const feedService = require('./feedService');
 
 const port = process.env.PORT || 3000;
